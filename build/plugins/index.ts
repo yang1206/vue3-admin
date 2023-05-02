5797991ecbf6217705a2ecb3c143bced97841a10
yang1206 <yang15188280673@outlook.com>
2023-05-02 14:25:38 +0800

    feat: pwa

--- build/plugins/index.ts
+++ build/plugins/index.ts
@@ -1,13 +1,42 @@
 import type { PluginOption } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import unocss from 'unocss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import { visualizer } from 'rollup-plugin-visualizer'
 import viteCompression from 'vite-plugin-compression'
 import unplugins from './unplugin'
 import { setupMockPlugin } from './mock'
 
 export function setupVitePlugins(viteEnv: ViteEnv, isBuild: boolean): PluginOption[] {
-  const plugins = [vue(), ...unplugins, unocss()]
+  const plugins = [
+    vue(),
+    VitePWA({
+      injectRegister: 'auto',
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Vue3 Admin',
+        short_name: 'Vue3 Admin',
+        description: 'Vue3 Admin description',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+    ...unplugins, unocss()]
   if (viteEnv.VITE_USE_MOCK)
     plugins.push(setupMockPlugin(isBuild))
 
