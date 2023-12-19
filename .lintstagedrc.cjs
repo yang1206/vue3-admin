df9c43a78e43e36511e82d8b963e54f231d2762d
yang1206 <yang15188280673@outlook.com>
2023-12-19 22:20:18 +0800

    fix: 修复 lint失效

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  '*.{js,cjs,mjs,jsx,ts,cts,mts,tsx,vue}': 'eslint --fix --cache',
+  '*.{js,cjs,mjs,jsx,ts,cts,mts,tsx,vue}': 'eslint --fix',
   '*.{scss,css,vue}': 'stylelint  --fix --allow-empty-input',
-  '*.{ts,cts,mts,tsx,vue}': () => 'vue-tsc --noEmit -p tsconfig.vitest.json --composite false',
+  '*.{ts,cts,mts,tsx,vue}': () => 'vue-tsc --noEmit --composite false',
 }
